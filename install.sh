e4a702c4fc0c1abf33d0b1ea8a5f01df432d288a
Anton Beloglazov <anton.beloglazov@gmail.com>
2014-07-15 12:19:30 +1000

    Added .ideavimrc

--- install.sh
+++ install.sh
@@ -4,12 +4,14 @@ cd $(dirname $BASH_SOURCE)
 
 mv -v ~/.vimrc ~/.vimrc.old 2> /dev/null
 mv -v ~/.vim ~/.vim.old 2> /dev/null
+mv -v ~/.ideavimrc ~/.ideavimrc.old 2> /dev/null
 
 export GIT_SSL_NO_VERIFY=true
 mkdir -p ~/.vim/autoload
 curl --insecure -fL -o ~/.vim/autoload/plug.vim https://raw.github.com/junegunn/vim-plug/master/plug.vim
 
 ln -sf `pwd`/.vimrc ~/.vimrc
+ln -sf `pwd`/.ideavimrc ~/.ideavimrc
 
 vim +PlugInstall +qall
 
