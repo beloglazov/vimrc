109310af3f1df51a64c161c93afeebaa0bba8239
Anton Beloglazov <anton.beloglazov@gmail.com>
2015-02-13 15:31:23 +1100

    Added a link to ~/.sshrc.d/.vimrc

--- install.sh
+++ install.sh
@@ -7,10 +7,12 @@ mv -v ~/.vim ~/.vim.old 2> /dev/null
 mv -v ~/.ideavimrc ~/.ideavimrc.old 2> /dev/null
 
 export GIT_SSL_NO_VERIFY=true
+mkdir -p ~/.sshrc.d
 mkdir -p ~/.vim/autoload
 curl --insecure -fL -o ~/.vim/autoload/plug.vim https://raw.github.com/junegunn/vim-plug/master/plug.vim
 
 ln -sf `pwd`/.vimrc ~/.vimrc
+ln -sf `pwd`/.vimrc ~/.sshrc.d/.vimrc
 ln -sf `pwd`/.ideavimrc ~/.ideavimrc
 
 vim +PlugInstall +qall
